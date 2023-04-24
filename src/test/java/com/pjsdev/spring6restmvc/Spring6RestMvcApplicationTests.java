8ddcf1db93c7120330bf4d36088eb230d4c3dffd
pjsdevcom <mail@pjsdev.com>
2023-04-24 08:45:50 +0100

    project init

---
+++ src/test/java/com/pjsdev/spring6restmvc/Spring6RestMvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pjsdev.spring6restmvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring6RestMvcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
