4bc6f943a74d2c9baea77501cf32f3f7b9384c1e
pjsdevcom <mail@pjsdev.com>
2023-05-05 09:21:20 +0100

    add devtools

--- src/main/java/com/pjsdev/spring6restmvc/services/CustomerServiceImpl.java
+++ src/main/java/com/pjsdev/spring6restmvc/services/CustomerServiceImpl.java
@@ -14,7 +14,7 @@ public class CustomerServiceImpl implements CustomerService {
     public CustomerServiceImpl() {
         Customer customer1 = Customer.builder()
                 .id(UUID.randomUUID())
-                .name("William Shakespeare")
+                .name("William Shakespeare the 2nd")
                 .version(1)
                 .dateCreated(LocalDateTime.now())
                 .dateUpdated(LocalDateTime.now())
