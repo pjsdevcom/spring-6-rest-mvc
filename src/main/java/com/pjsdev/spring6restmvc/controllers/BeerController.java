432a5a1a01351612e051bb7ac4effaa5bd71ddf8
pjsdevcom <mail@pjsdev.com>
2023-05-26 11:08:10 +0100

    configure MockMVC for beer controller

--- src/main/java/com/pjsdev/spring6restmvc/controllers/BeerController.java
+++ src/main/java/com/pjsdev/spring6restmvc/controllers/BeerController.java
@@ -2,7 +2,7 @@ package com.pjsdev.spring6restmvc.controllers;
 
 import com.pjsdev.spring6restmvc.model.Beer;
 import com.pjsdev.spring6restmvc.services.BeerService;
-import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
@@ -13,7 +13,7 @@ import java.util.List;
 import java.util.UUID;
 
 @Slf4j
-@AllArgsConstructor
+@RequiredArgsConstructor
 @RestController
 @RequestMapping("/api/v1/beers")
 public class BeerController {
