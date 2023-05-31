28fcc3a4c66eb6d25611af7501af08aa63a952b4
pjsdevcom <mail@pjsdev.com>
2023-05-31 10:13:31 +0100

    add mockito config

--- src/test/java/com/pjsdev/spring6restmvc/controllers/BeerControllerTest.java
+++ src/test/java/com/pjsdev/spring6restmvc/controllers/BeerControllerTest.java
@@ -1,6 +1,8 @@
 package com.pjsdev.spring6restmvc.controllers;
 
+import com.pjsdev.spring6restmvc.model.Beer;
 import com.pjsdev.spring6restmvc.services.BeerService;
+import com.pjsdev.spring6restmvc.services.BeerServiceImpl;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
@@ -8,10 +10,13 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 
 import java.util.UUID;
 
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(BeerController.class) //indicating this is a test splice, limited to BeerController class
@@ -23,10 +28,17 @@ class BeerControllerTest {
     @MockBean
     BeerService beerService;
 
+    BeerServiceImpl beerServiceImpl = new BeerServiceImpl();
+
     @Test
     void getBeerById() throws Exception {
 
+        Beer testBeer = beerServiceImpl.listBeers().get(0);
+
+        given(beerService.getBeerById(any(UUID.class))).willReturn(testBeer);
+
         mockMvc.perform(get("/api/v1/beers/" + UUID.randomUUID()).accept(MediaType.APPLICATION_JSON))
-                .andExpect(status().isOk());
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON));
     }
 }
