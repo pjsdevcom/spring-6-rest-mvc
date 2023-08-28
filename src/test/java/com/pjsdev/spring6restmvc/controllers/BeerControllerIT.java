77dfa3376d94b6ab8cdf9bbe00dc32a92d7b7a89
pjsdevcom <mail@pjsdev.com>
2023-08-28 21:34:53 +0100

    add integration tests for beer controller list operation

---
+++ src/test/java/com/pjsdev/spring6restmvc/controllers/BeerControllerIT.java
@@ -0,0 +1,40 @@
+package com.pjsdev.spring6restmvc.controllers;
+
+import com.pjsdev.spring6restmvc.model.BeerDTO;
+import com.pjsdev.spring6restmvc.repositories.BeerRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+class BeerControllerIT {
+
+    @Autowired
+    BeerController beerController;
+
+    @Autowired
+    BeerRepository beerRepository;
+
+    @Test
+    void testListBeers() {
+        List<BeerDTO> dtos = beerController.listBeers();
+
+        assertThat(dtos.size()).isEqualTo(3);
+    }
+
+    @Rollback //to return DB to the original state
+    @Transactional
+    @Test
+    void testEmptyList() {
+        beerRepository.deleteAll();
+        List<BeerDTO> dtos = beerController.listBeers();
+
+        assertThat(dtos.size()).isEqualTo(0);
+    }
+}
