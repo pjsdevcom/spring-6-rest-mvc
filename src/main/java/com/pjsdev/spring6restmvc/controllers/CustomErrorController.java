d4c6c16e6e69726e3b0de8f9bf798cd436a9afa4
pjsdevcom <mail@pjsdev.com>
2023-09-25 10:57:29 +0100

    add JPA validation error handler

--- src/main/java/com/pjsdev/spring6restmvc/controllers/CustomErrorController.java
+++ src/main/java/com/pjsdev/spring6restmvc/controllers/CustomErrorController.java
@@ -1,6 +1,7 @@
 package com.pjsdev.spring6restmvc.controllers;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.transaction.TransactionSystemException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -22,5 +23,10 @@ public class CustomErrorController {
         }).toList();
 
         return ResponseEntity.badRequest().body(errorList);
+    }
+
+    @ExceptionHandler(TransactionSystemException.class)
+    ResponseEntity handleJPAViolations(TransactionSystemException exception) {
+        return ResponseEntity.badRequest().build();
     }
 }
